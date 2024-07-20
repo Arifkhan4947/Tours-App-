bf5f1af8dddf22a6f2520c734c3130e92628bcf7
Arif Khan <khanarif2738@gmail.com>
2024-07-20 22:24:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import data from './data';
+import Tours from './components/Tours';
 
 function App() {
+
+  const [tours, setTours] = useState(data)
+
+  function removeTour(id) {
+    const newTours = tours.filter (tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  // if UI having not any tours card show then show this function value on the UI
+  if(tours.length ===0) {
+    return (
+        <div className='refresh'>
+          <h2 className="heading">No Tours Left</h2>
+          <button className="btn-white" onClick={ () => setTours(data)}>Refresh</button>
+        </div>
+    );
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <Tours tours={tours} removeTour={removeTour}></Tours>
+      </div>
+    
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,55 @@
+
+const data = [
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra is a historic city located in the northern state of Uttar Pradesh, India. It is renowned for its rich Mughal heritage and is home to some of the most iconic monuments in the world.  With a population of roughly 1.6 million, Agra is the fourth-most populous city in Uttar Pradesh",
+        image:"https://plus.unsplash.com/premium_photo-1661885523029-fc960a2bb4f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"35,758",
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur, also known as the “Pink City,” is the capital and largest city of the Indian state of Rajasthan. It is famous for its rich history, vibrant culture, and stunning architecture. Jaipur is also known as the Pink City due to the dominant color scheme of its buildings in old city",
+        image:"https://images.unsplash.com/photo-1602643163983-ed0babc39797?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"65,500",
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa is a beautiful state located on the western coast of India, known for its stunning beaches, and rich cultural heritage. It is the second-highest ranking among Indian states.",
+        image:"https://images.unsplash.com/photo-1642922835816-e2ac68db5c42?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"75,450",
+    },
+    {
+        id:4,
+        name:"Varanasi",
+        info:"Varanasi, also known as Benares or Kashi, is one of the oldest continuously inhabited cities in the world and is located in the northern Indian state of Uttar Pradesh. It is situated on the banks of the Ganges River and is considered one of the holiest cities in Hinduism.",
+        image:"https://images.unsplash.com/photo-1708961370545-5f00499a1808?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"45,550",
+    },
+    {
+        id:5,
+        name:"Darjeeling",
+        info:"Darjeeling, often referred to as the “Queen of the Hills,” is a charming hill station located in the northern part of West Bengal, India. It is renowned for its stunning landscapes, tea plantations, and colonial-era architecture.",
+        image:"https://images.unsplash.com/photo-1647157438772-3841f0d41f77?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"72,999",
+    },
+    {
+        id:6,
+        name:"London",
+        info:"London is the capital and largest city of both England and the United Kingdom. It is a vibrant metropolis known for its rich history, diverse culture, and significant global influence. London is a major global centre of higher education teaching and research and has the largest concentration of higher education institutes in Europe.",
+        image:"https://images.unsplash.com/photo-1502700559166-5792585222ef?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"99,999",
+    },
+    {
+        id:7,
+        name:"Dubai",
+        info:"Dubai is a vibrant and cosmopolitan city located in the United Arab Emirates (UAE). It is known for its modern architecture, luxury shopping, and lively nightlife scene. Here are some key highlights about Dubai:",
+        image:"https://images.unsplash.com/photo-1518684079-3c830dcef090?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"85,999",
+    },
+
+]
+
+export default data;
